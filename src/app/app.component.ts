56d3d3748f8198e9756827beb73d89071692eaed
MohamedSalman2371999 <MohamedSalman2371999@github.com>
2024-09-08 17:37:16 +0300

    E-commerce angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgxSpinnerComponent],
+  imports: [RouterOutlet, NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'E-commerce';
+  private readonly _PLATFORM_ID = inject(PLATFORM_ID)
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    if (isPlatformBrowser(this._PLATFORM_ID)) {
+      localStorage.setItem("lang", 'en')
+    }
+  }
+
 }
